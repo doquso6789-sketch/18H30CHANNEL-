2f03f364f8164bfd9a350bab06dbd7310f41663a
doquso <doquso6789@gmail.com>
2026-06-22 02:36:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from ai import train, predict
+from collector import crawl
+import asyncio
 
+app = FastAPI()
+
+model, data = train()
+
+@app.get("/")
+def home():
+    return HTMLResponse(open("templates/dashboard.html","r",encoding="utf-8").read())
+
+@app.websocket("/ws")
+async def ws(websocket: WebSocket):
+
+    await websocket.accept()
+
+    global data
+
+    while True:
+
+        try:
+            new_data = crawl()
+
+            if new_data:
+                data = new_data
+
+            result = predict(model, data)
+
+            await websocket.send_json(result)
+
+            await asyncio.sleep(5)
+
+        except:
+            await asyncio.sleep(5)
